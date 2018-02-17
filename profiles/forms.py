298db60671a9e6e31b119d04428ceeb8333dc0a1
Mike M <mikemaddem@gmail.com>
2018-02-17 13:12:12 -0500

    Forms Initial Commit

---
+++ profiles/forms.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django import forms
+from .models import UserProfile
+
+class CreateUserForm(forms.ModelForm):
+    email = forms.EmailField(max_length=200, help_text='Required')
+    password = forms.CharField(widget=forms.PasswordInput)
+    tos = forms.BooleanField(required=True)
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password']
+
+class EditProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = (
+            #'profile_picture',
+            'about_me',
+        )
