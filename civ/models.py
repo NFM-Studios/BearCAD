54b0e8d632ded1617f175d6e6f527d0cfe8f8098
Mike M <mikemaddem@gmail.com>
2018-02-22 16:41:28 -0500

    Progress on the civ model

--- civ/models.py
+++ civ/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Civilian(models.Model):
+    LICENSE_STATUS_CHOICES = (
+        ('revoked', 'Revoked'),
+        ('valid', 'Valid'),
+        ('expired', 'expired'),
+        ('none', 'None')
+    )
+    name = models.
+    driving_license = models.CharField(max_length=10, choices=LICENSE_STATUS_CHOICES, default=none)
+    hunting_license = models.CharField(max_length=10, choices=LICENSE_STATUS_CHOICES, default=none)
+    boating_license = models.CharField(max_length=10, choices=LICENSE_STATUS_CHOICES, default=none)
+    weapon_permit = models.CharField(max_length=10, choices=LICENSE_STATUS_CHOICES, default=none)
+
+
+class Vehicle(models.Model):
+    MAKE_MODEL_CHOICES = (
+        ('albany')
+    )
+    make_model = models.CharField(max_length=20, )
