f30c56e727042c9393f055d201f6ccd8a1d5ab34
Mike M <mikemaddem@gmail.com>
2018-02-23 12:25:19 -0500

    swapped default values for licenses

--- civ/models.py
+++ civ/models.py
@@ -10,15 +10,15 @@ class Civilian(models.Model):
         ('expired', 'expired'),
         ('none', 'None')
     )
-    name = models.
-    driving_license = models.CharField(max_length=10, choices=LICENSE_STATUS_CHOICES, default=none)
-    hunting_license = models.CharField(max_length=10, choices=LICENSE_STATUS_CHOICES, default=none)
-    boating_license = models.CharField(max_length=10, choices=LICENSE_STATUS_CHOICES, default=none)
-    weapon_permit = models.CharField(max_length=10, choices=LICENSE_STATUS_CHOICES, default=none)
+    #name = models.
+    driving_license = models.CharField(max_length=10, choices=LICENSE_STATUS_CHOICES, default=None)
+    hunting_license = models.CharField(max_length=10, choices=LICENSE_STATUS_CHOICES, default=None)
+    boating_license = models.CharField(max_length=10, choices=LICENSE_STATUS_CHOICES, default=None)
+    weapon_permit = models.CharField(max_length=10, choices=LICENSE_STATUS_CHOICES, default=None)
 
 
 class Vehicle(models.Model):
     MAKE_MODEL_CHOICES = (
         ('albany')
     )
-    make_model = models.CharField(max_length=20, )
+    #make_model = models.CharField(max_length=20, )
