b3627465801f54a431946680bb60803796b0d055
Mike M <mikemaddem@gmail.com>
2019-11-19 15:49:12 -0500

    modified profile models

--- profiles/models.py
+++ profiles/models.py
@@ -1,22 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.db.models.signals import post_save
+from cad.models import Department, UserVehicle
 
-# Create your models here.
+
+class PersonProfile(models.Model):
+    first = models.CharField(default="First", max_length=20)
+    last = models.CharField(default="Last", max_length=20)
+    departments = models.ManyToManyField(Department)
+    # warrants = mtm
+    vehicles = models.ManyToManyField(UserVehicle)
+    # addresses
+    # permits
+
 
 class UserProfile(models.Model):
     def __str__(self):
         return str(self.user)
+
     user = models.OneToOneField(User, related_name='user', on_delete=models.CASCADE)
-    #email_verified = models.BooleanField(default=False)
+    # email_verified = models.BooleanField(default=False)
     about_me = models.CharField(max_length=500, default='Forever a mystery', blank=True)
-    identifier = models.CharField(max_length=25, default='CIV-?')
-    civ = models.BooleanField(default=False)
-    police = models.BooleanField(default=False)
-    ems = models.BooleanField(default=False)
-    fire = models.BooleanField(default=False)
     approved = models.BooleanField(default=False)
     user_type = models.CharField(max_length=10, default='user')
+    persons = models.ManyToManyField(PersonProfile)
+    bio = models.TextField(default="User hasn't set a bio yet")
+
 
 def create_profile(sender, **kwargs):
     user = kwargs["instance"]
@@ -24,5 +32,4 @@ def create_profile(sender, **kwargs):
         user_profile = UserProfile(user=user)
         user_profile.save()
 
-
-#post_save.connect(create_profile, sender=User)
+# post_save.connect(create_profile, sender=User)
