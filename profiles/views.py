4e04f8fda17e8416b125fe93afdecd7b0cdacbe8
Mike M <mikemaddem@gmail.com>
2019-11-18 22:03:19 -0500

    remove account tokens temporarily from profile views

--- profiles/views.py
+++ profiles/views.py
@@ -13,7 +13,7 @@ from django.contrib.sites.shortcuts import get_current_site
 from django.utils.encoding import force_bytes, force_text
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode, is_safe_url
 from django.template.loader import render_to_string
-from .tokens import account_activation_token
+#from .tokens import account_activation_token
 from django.core.mail import EmailMessage
 from django.forms.models import inlineformset_factory
 from django.core.exceptions import PermissionDenied
