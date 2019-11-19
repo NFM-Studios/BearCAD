2ceb37a1f3a042089c7b628976161976f6415fdb
Mike M <mikemaddem@gmail.com>
2019-11-19 15:24:34 -0500

    warning, citation, department, street, and call models initial commit

--- cad/models.py
+++ cad/models.py
@@ -1,3 +1,115 @@
 from django.db import models
+from profiles.models import PersonProfile
 
-# Create your models here.
+
+class UserVehicleMakeChoice(models.Model):
+    name = models.CharField(default="vehicle make", max_length=255)
+
+
+class UserVehicleModelChoice(models.Model):
+    name = models.CharField(default="vehicle model", max_length=255)
+
+
+class UserVehicle(models.Model):
+    make = models.CharField(choices=UserVehicleMakeChoice, max_length=255)
+    model = models.CharField(choices=UserVehicleModelChoice, max_length=255)
+
+
+class MapCountyOption(models.Model):
+    name = models.CharField(default="CountyOption", max_length=255)
+
+
+
+class MapStreetOption(models.Model):
+    name = models.CharField(default="StreetOption", max_length=255)
+    county = models.ForeignKey(MapCountyOption, related_name='street_county', on_delete=models.SET_NULL)
+    postal = models.CharField(null=True, blank=True, max_length=10)
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=255, default="Default Department Name")
+    short = models.CharField(null=True, blank=True, max_length=255)
+    max_size = models.PositiveSmallIntegerField(null=True, blank=True)
+    description = models.TextField(default="No department description")
+    # TODO - LOGO/Image Field for Department
+
+
+class UnitStatusOption(models.Model):
+    code = models.CharField(default="10-8", max_length=20)
+    description = models.CharField(default="Available", max_length=255)
+
+
+class DepartmentUnit(models.Model):
+    callsign = models.CharField(default='1A-25', max_length=50)
+    status = models.PositiveSmallIntegerField(default=0)
+
+
+class CallReport(models.Model):
+    info = models.TextField(default='Call Report')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Call(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    # TODO - verify on_delete set null
+    creator = models.ForeignKey(PersonProfile, related_name='call_creator', on_delete=models.SET_NULL)
+    departments = models.ManyToManyField(Department)
+    reports = models.ManyToManyField(CallReport)
+    street1 = models.ForeignKey(MapStreetOption, related_name='MAP_STREET_1', on_delete=models.SET_NULL)
+    street2 = models.ForeignKey(MapStreetOption, related_name='MAP_STREET_2', on_delete=models.SET_NULL)
+    street_other = models.CharField(blank=True, Null=True, max_length=255)
+    units =
+
+
+class CitationType(models.Model):
+    name = models.CharField(default="Citation Type", max_length=100)
+    description = models.CharField(default="Citation Description", max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Citation(models.Model):
+    person = models.ForeignKey(PersonProfile, related_name='citation_person', on_delete=models.SET_NULL)
+    creator = models.ForeignKey(PersonProfile, related_name='citation_creator', on_delete=models.SET_NULL)
+    department_issuing = models.ForeignKey(Department, related_name='issuing_department', on_delete=models.SET_NULL)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    description = models.TextField(default="no warning description description")
+    type = models.ManyToManyField(CitationType)
+
+
+class WarningType(models.Model):
+    name = models.CharField(default="Warning Type", max_length=100)
+    description = models.CharField(default="Warning Description", max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Warning(models.Model):
+    person = models.ForeignKey(PersonProfile, related_name='warning_person', on_delete=models.SET_NULL)
+    creator = models.ForeignKey(PersonProfile, related_name='warning_creator', on_delete=models.SET_NULL)
+    department_issuing = models.ForeignKey(Department, related_name='issuing_department', on_delete=models.SET_NULL)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    description = models.TextField(default="no warning description description")
+    type = models.ManyToManyField(WarningType)
+
+
+class WarrantType(models.Model):
+    name = models.CharField(default="Warrant Type", max_length=100)
+    description = models.CharField(default="Warrant Description", max_length=255)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+
+class Warrant(models.Model):
+    person = models.ForeignKey(PersonProfile, related_name='wanted_person', on_delete=models.SET_NULL)
+    creator = models.ForeignKey(PersonProfile, related_name='warrant_creator', on_delete=models.SET_NULL)
+    department_issuing = models.ForeignKey(Department, related_name='issuing_department', on_delete=models.SET_NULL)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    description = models.TextField(default="no warrant description")
+    type = models.ManyToManyField(WarrantType)
+    call = models.ManyToManyField(Call)
